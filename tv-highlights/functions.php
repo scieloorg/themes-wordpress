1751b7bb3a9b63c24515d93856626366cb567487
jct-ux <juliotak@me.com>
2014-11-10 19:19:39 -0200

    primeiro commit para o tema tv-highlights

---
+++ tv-highlights/functions.php
@@ -0,0 +1,27 @@
+<?php
+	register_sidebar( array(
+		'name'          => 'home-highlights',
+		'id'            => 'home-highlights',
+		'before_widget' => '<div id="%1$s" class="widget %2$s">',
+		'after_widget'  => '</div>',
+		'before_title'  => '<h1 class="widget-title">',
+		'after_title'   => '</h1>',
+	) );
+	register_sidebar( array(
+		'name'          => 'tv',
+		'id'            => 'tv',
+		'before_widget' => '<div id="%1$s" class="widget %2$s">',
+		'after_widget'  => '</div>',
+		'before_title'  => '<h1 class="widget-title">',
+		'after_title'   => '</h1>',
+	) );
+	register_sidebar( array(
+		'name'          => 'weather',
+		'id'            => 'weather',
+		'before_widget' => '<div id="%1$s" class="widget %2$s">',
+		'after_widget'  => '</div>',
+		'before_title'  => '<h1 class="widget-title">',
+		'after_title'   => '</h1>',
+	) );
+
+?>
